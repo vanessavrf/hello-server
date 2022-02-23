41f057fa7cddeff12fdb33e04c5d944031a8042d
vanrfernandes <99264747+vanrfernandes@users.noreply.github.com>
2022-02-23 11:27:02 -0300

    tudobelezinha

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 'use strict'
 
-const greeting = "Bom dia! "  // Change this line to change your greeting
+const greeting = "Tudo belezinha "  // Change this line to change your greeting
 
 const port = (typeof process.env.PORT !== 'undefined')
   ? process.env.PORT
