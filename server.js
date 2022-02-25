953f22945cabcd2f6420823e64f074bc8ee7ae3f
vanrfernandes <99264747+vanrfernandes@users.noreply.github.com>
2022-02-25 18:29:54 -0300

    ola v1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 'use strict'
 
-const greeting = "Tudo belezinha v2"  // Change this line to change your greeting
+const greeting = "Ola v1"  // Change this line to change your greeting
 
 const port = (typeof process.env.PORT !== 'undefined')
   ? process.env.PORT
